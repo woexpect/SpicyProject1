7b59d9a8f074cd8233882403a8fbe3a69fb7235a
woexpect <sebasp95@homtail.com>
2017-04-07 13:35:56 -0500

    Initial commit

---
+++ src/main/java/co/com/spicy/Project1Application.java
@@ -0,0 +1,12 @@
+package co.com.spicy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project1Application.class, args);
+	}
+}
